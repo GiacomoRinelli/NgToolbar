5dbf051ea8b85e787640c2e9249dc6adb433adff
GiacomoRinelli <103842934+GiacomoRinelli@users.noreply.github.com>
2022-11-23 10:20:29 +0100

    Hopefully works

---
+++ src/app/Routing/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+/* Importo i componenti che voglio utilizzare all'interno del mio sistema di routing */
+import { HomePageComponent } from '../Components/HomePageComponent/home-page.component';
+import { FollowComponent } from '../Components/FollowComponent/follow.component';
+import { ShareComponent } from '../Components/ShareComponent/share.component';
+
+/* path: tutto ciò che viene dopo lo slash nello url. Una volta che mi trovo su quella specifica url
+   viene renderizzato esattamente il contenuto del componente corrispondente.
+   Se mi trovo su /home , avrò le informazioni riguardanti il componente HomePageComponent */
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'follow', component: FollowComponent },
+  { path: 'share', component: ShareComponent },
+];
+
+/* Roba abbastanza standard da impostare */
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './Routing/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Components Imports (Stesso ragionamento dello using per ciccio sciarpio) */
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Components/HomePageComponent/home-page.component';
+import { FollowComponent } from './Components/FollowComponent/follow.component';
+import { ShareComponent } from './Components/ShareComponent/share.component';
+
+/* Angular Material Imports (Ang mat è una libreria da importare, per fare roba esteticamente più figa)*/
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+/* Nelle declarations vengono dichiarati i componenti che vengono utilizzati all'interno dell'applicazione. */
+/* In questo caso, i componenti HomePageComponent, FollowComponent, ShareComponent viene utilizzato all'interno del componente AppComponent. */
+/* Negli imports vengono dichiarati i pacchetti che andremo ad utilizzare */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FollowComponent,
+    ShareComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
