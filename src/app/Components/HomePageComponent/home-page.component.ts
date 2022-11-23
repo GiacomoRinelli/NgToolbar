6c4615770c3e1d97fada088ff97696b57712736d
GiacomoRinelli <103842934+GiacomoRinelli@users.noreply.github.com>
2022-11-23 12:43:21 +0100

    Animation Added

--- src/app/Components/HomePageComponent/home-page.component.ts
+++ src/app/Components/HomePageComponent/home-page.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          height: '200px',
+          opacity: 1,
+          backgroundColor: '#3399FF',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '100px',
+          opacity: 0.5,
+          backgroundColor: 'blueviolet',
+        })
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
+    ]),
+  ],
 })
 export class HomePageComponent {
-
   showContent = true;
 
   people = [
@@ -29,4 +57,10 @@ export class HomePageComponent {
       employed: false,
     },
   ];
+
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
